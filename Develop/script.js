7ac70332a335fc6b0634469703f99e2ddf3f5a89
V Mendez <victormendez990@gmail.com>
2022-09-20 02:04:29 -0600

    Add Prompts and validation

--- Develop/script.js
+++ Develop/script.js
@@ -7,23 +7,39 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "=", ",", "<", ">", "?"]
 
 function generatePassword() {
-  var charsToUse = [];
-
-  //Ask User if they would like to include numbers in their password
-  var 
-
-  //Ask User if they would like to include lower case letters in their password
-  var
-
-  //Ask User if they would like to include upper case letters in their password
-  var
-
-  //Ask User if they would like to include special characters in their password
-  var
 
   //Ask User for desired password length
-  var 
+  var Length = parseInt(prompt("Password Length? (8-128 characters)"))
 
+  //Ask User if they would like to include numbers in their password
+  var incNums = confirm("Include numbers in password?")
+
+  //Ask User if they would like to include lower case letters in their password
+  var incLowerCase = confirm("Include lower case letters in password?")
+
+  //Ask User if they would like to include upper case letters in their password
+  var incUpperCase = confirm("Include Upper case letters in password?")
+
+  //Ask User if they would like to include special characters in their password
+  var incSpecialChar = confirm("Include special characters?")
+
+
+  //Console log for testing
+  console.log(Length, incNums, incLowerCase, incUpperCase, incSpecialChar)
+
+  //Verify user typed number within parameters
+  if(isNaN(Length)==true || Length < 8 || Length > 128) {
+    alert('Length must be between 8 and 128 characters. Try Again!')
+    generatePassword()
+  }
+
+  //Verify user chose a type of character
+  if(incNums == false && incLowerCase == false && incUpperCase == false && incSpecialChar == false) {
+    alert('At least one character type must be chosen. Try Again!')
+    generatePassword()
+  }
+
+  var charsToUse = [];
 }
 
 // Get references to the #generate element
